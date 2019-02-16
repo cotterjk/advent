0b198e22cac37be86a8b3fbdbeeca80f78e45bdb
Cotter <cotterjk@gmail.com>
2019-02-16 11:41:12 -0500

    Added functioned for calculating today's day of Advent, refactored other functions to depend on it

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var day_num = 0;
-
-
-
 var liturgy = [
    {
        "titleText" : "Day 00: It's not Advent",
@@ -34,13 +30,30 @@ var liturgy = [
 ///////////////////////////////////////
 ///////////////////////////////////////
 
-function loadLiturgy() {
+function loadLiturgy(day_num) {
     document.getElementById('day-title').innerHTML = liturgy[day_num].titleText;
     document.getElementById('entry').innerHTML = liturgy[day_num].entryText;
 }
 
-function loadCalendar() {
+function loadCalendar(day_num) {
     document.getElementById('calendar-img').src = ("assets/images/" + liturgy[day_num].imgName);
+}
+
+
+function dayOfAdvent() {
+    var day_num = 0;
+    var today = new Date();
+    // console.log ("new Date() returned: " + today);
+    // console.log ("today.getMonth() returned: " + today.getMonth());
+    // console.log ("today.getDate() returned: " + today.getDate());
+    if (today.getMonth() == 11 && today.getDate() < 26) { //zero-indexed, Dec is 11.
+        console.log ("It's Advent.");
+        day_num = today.getDate();
+        console.log ("It's day " + today.getDate() + ".");
+
+    }
+    day_num = 1; // TESTING OVERRIDE until all days have images and entries
+    return day_num;
 }
 
 
@@ -48,8 +61,9 @@ $( document ).ready(function() {
     console.log("### Loading page")
     ///
     ///
-    loadCalendar();
-    loadLiturgy();
+    var day_num = dayOfAdvent();
+    loadCalendar(day_num);
+    loadLiturgy(day_num);
     ///
     ///
     console.log("### $( document ).ready(function() all executed")
