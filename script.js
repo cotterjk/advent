c3a92494cfabcfcf698eabcb2e8b6afacea4a5ca
Cotter <cotterjk@gmail.com>
2019-08-15 21:20:57 -0400

    Edited animations to fade in two calendar images on top of each other

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var liturgy = [
    {
-       "titleText" : "Day 00: It's not Advent",
+       "titleText" : "It's not Advent.",
 
-        "imgName" : "day00.png",
+        "imgName" : "00.png",
 
        "entryText" : "Come back on Dec. 1"
    },
@@ -11,7 +11,7 @@ var liturgy = [
    {
        "titleText" : "Day 01:<br/>The Turtle",
 
-       "imgName" : "day01.png",
+       "imgName" : "01.png",
 
       "entryText" : "Turtle lets go everything she knows,<br/>everything she loves,<br/>and sinks down, way down<br/>into the dark beneath the pond.<br/>She rests utterly in God’s care.<br/>She is lost to all the self<br/>she has ever known.<br/>She cannot protect herself.<br/>She can only be still and wait.<br/>And from that darkness,<br/>that utter still and perfect<br/>waiting.<br/>she may be reborn<br/>when the earth warms<br/>and the sun grows stronger<br/>again and begins its journey<br/>to the summer house.<div class='entrySource'>—letter from a friend</div>"
     },
@@ -19,7 +19,7 @@ var liturgy = [
     {
         "titleText" : "Day 02:<br/>The Snake",
 
-        "imgName" : "day02.png",
+        "imgName" : "02.png",
 
         "entryText" : "At night<br/>under the trees<br/>the black snake<br/>jellies forward<br/>rubbing<br/>roughly<br/>the stems of the bloodroot,<br/>the yellow leaves,<br/>little boulders of bark,<br/>to take off<br/>the old life.<br/>I don’t know<br/>if he knows<br/>it will work.<br/>In the distance<br/>the moon and the stars<br/>give a little light.<br/>In the distance<br/>the owl cries out.<br/><br/>In the distance<br/>the owl cries out.<br/>The snake knows<br/>these are the owl’s woods,<br/>these are the woods of death,<br/>these are the woods of hardship<br/>where you crawl and rawl,<br/>where you live in the husks of trees,<br/>where you lie on the wild twigs<br/>and the cannot bear your weight,<br/>where life has no purpose<br/>and is neither civil nor intelligent.<br/><br/>Where life has no purpose,<br/>and is neither civil nor intelligent,<br/>it begins<br/>to rain,<br/>it begins<br/>to smell like the bodies<br/>of flowers.<br/>At the back of the neck<br/>the old skin splits.<br/>The snake shivers<br/>but does not hesitate.<br/>He inches forward.<br/>He begins to bleed through<br/>like satin.<div class='entrySource'>—“The Forest” from “Rain” by Mary Oliver</div>"
     },
@@ -27,7 +27,7 @@ var liturgy = [
     {
         "titleText" : "Day 03:<br/>The Fire in the Center of the Earth",
 
-        "imgName" : "day03.png",
+        "imgName" : "03.png",
 
         "entryText" : "Earth grown old, yet still so green,<br/>Deep beneath her crust of cold<br/>Nurses fire unfelt, unseen:<br/>Earth grown old.<br/><br/>We live are quickly told:<br/>Mission more lie hid between<br/>Inner swathings of her fold.<br/><br/>When will fire break up her screen?<br/>When will life burst thro’ her mould?<br/>Earth, earth, earth, thy cold is keen,<br/>Earth grown old.<div class='entrySource'>—from “Advent” by Christina Rossetti</div>"
     },
@@ -35,7 +35,7 @@ var liturgy = [
     {
         "titleText" : "Day 04:<br/>The Bear",
 
-        "imgName" : "day04.png",
+        "imgName" : "04.png",
 
         "entryText" : "Where is the sound?<br/>Where is the full, round<br/>	heart-beating sound?<br/>I have heard another life<br/>	It is dark inside<br/>It will get a great deal darker<br/>I have heard another life<br/>	Buried deep beneath layers of dead leaves<br/>	in dark ground<br/>	the full, round heart-<br/>	beating sound<br/>I have heard another life<br/>	Outside<br/>	it has just now<br/>	begun<br/>	to snow<br/>I have heard another life<br/>	wake in me.<div class='entrySource'>—from “Passages” by Cheryl Hellner</div>"
     },
@@ -43,7 +43,7 @@ var liturgy = [
     {
         "titleText" : "Day 05:<br/>The Petrel and The Porpoise",
 
-        "imgName" : "day05.png",
+        "imgName" : "05.png",
 
         "entryText" : "Love is most nearly itself<br/>When here and now cease to matter.<br/>Old men ought to be explorers<br/>Here or there does not matter<br/>We must be still and still moving<br/>Into another intensity<br/>For a further union, a deeper communion<br/>Through the dark cold and the empty desolation,<br/>The wave cry, the wind cry, the vast waters<br/>Of the petrel and the porpoise. In my end is my beginning.<div class='entrySource'>—from “East Coker” in the “Four Quartets” by TS Eliot</div>"
     },
@@ -51,7 +51,7 @@ var liturgy = [
     {
         "titleText" : "Day 06:<br/>The Seed",
 
-        "imgName" : "day06.png",
+        "imgName" : "06.png",
 
         "entryText" : "Truly, I say to you:<br/>unless a seed of wheat fall<br/>to the ground and die,<br/>it remains alone; but if it die,<br/>it brings forth much fruit.<div class='entrySource'>—John 12:24</div>"
     },
@@ -59,7 +59,7 @@ var liturgy = [
     {
         "titleText" : "Day 07:<br/>The Hands Outstretched",
 
-        "imgName" : "day07.png",
+        "imgName" : "07.png",
 
         "entryText" : "Thanks be to God for this season<br/>of darkness and cold<br/>when all comforts, new and old,<br/>fall away and all we have left<br/>is hunger‚<br/>hunger too deep to be fed<br/>by any but the Living Bread.<br/><br/>Hands outstretched,<br/>every fiber awake,<br/>we wait‚<br/>We shall not be fed<br/>by any but the Living Bread.<div class='entrySource'>—Potter’s House Church Advent liturgy, 1993</div>"
     },
@@ -67,7 +67,7 @@ var liturgy = [
     {
         "titleText" : "Day 08:<br/>The Rain",
 
-        "imgName" : "day08.png",
+        "imgName" : "08.png",
 
         "entryText" : "(Thus says the Holy One:) Though your God gave you the bread of adversity and the water of affliction, yet no more will your teacher be hidden, but your eyes shall see your teachers; and your ears shall hear a word behind you saying: “This is the way, walk in it,” when you turn to the right and when you turn to the left. Then shall the Holy One give rain to your seed and bread from the earth, and it shall be rich and abundant.<div class='entrySource'>—Isaiah 30:20, 21, 23</div>"
     },
@@ -75,7 +75,7 @@ var liturgy = [
     {
         "titleText" : "Day 09:<br/>The Rainbow",
 
-        "imgName" : "day09.png",
+        "imgName" : "09.png",
 
         "entryText" : "Earth is your mother,<br/>she holds you.<br/>Sky is your father,<br/>he protects you.<br/>Sleep,<br/>sleep.<br/>Rainbow is your sister,<br/>she loves you.<br/>The winds are your brothers,<br/>they sing to you.<br/>Sleep,<br/>sleep.<br/>We are together always.<br/>We are together always.<br/>There never was a time<br/>when this<br/>was not so.<div class='entrySource'>—Native American song, quoted by Leslie Marmon Silko</div>"
     },
@@ -83,7 +83,7 @@ var liturgy = [
     {
         "titleText" : "Day 10:<br/>The Sun",
 
-        "imgName" : "day10.png",
+        "imgName" : "10.png",
 
         "entryText" : "O Morning Star,<br/>O Radiant Dawn,<br/>When will we sing<br/>your morning song?<div class='entrySource'>—Friedrich Spee, 17th century</div>"
     },
@@ -91,7 +91,7 @@ var liturgy = [
     {
         "titleText" : "Day 11:<br/>The Stars",
 
-        "imgName" : "day11.png",
+        "imgName" : "11.png",
 
         "entryText" : "Creator of the stars of night,<br/>Your people’s everlasting light,<br/>O Christ, Redeemer of us all,<br/>We pray you hear us when we call.<br/><br/>Come, Sun and Savior, to embrace<br/>Our gloomy world, its weary race,<br/>As groom to bride, as bride to groom.<br/>The wedding chamber, Mary’s womb.<div class='entrySource'>—Advent hymn of the 8th century</div>"
     },
@@ -99,7 +99,7 @@ var liturgy = [
     {
         "titleText" : "Day 12:<br/>Our Lady of Guadalupe",
 
-        "imgName" : "day12.png",
+        "imgName" : "12.png",
 
         "entryText" : "A long time ago, in the country called Mexico, there lives an Indian, Juan Diego. Every week he walked from his village to the Church of Santiago in the city to attend mass and praise God with the great company.<br/><br/>On the 9th of December he set out as usual before dawn, wrapping his tilma about him for warmth. But this day as Juan Diego neared the hill of Tepeyac, he heard singing. A brilliant cloud covered the top of the hill. Juan Diego went closer, and the cloud seemed to explode in a rainbow of color.<br/><br/>Then a gentle woman’s voice spoke to him in his native language: “Juan, Juan Diego.” Juan Diego ran to the top of the hill. As he reached the summit, the cloud parted, and he saw the most beautiful woman dressed in the robes of an Indian princess.<br/><br/>Lights shone out in rays around her. “Juan Diego,” said the woman, “smallest and most beloved of my song. I am the Mother of God. Go to the bishop at the great church; tell him I’ve sent you. Tell him I want a church built where I can show my love to your people.”<br/><br/>Juan Diego saw the Lady stood on a crescent moon held by an angel with colored wings. He fell to the ground and covered his head with his tilma.<br/><br/>“My son,” said the Lady in a gentle voice, “climb to the top of the hill. Cut and gather the roses there. Collect them in your tilma and bring them to the bishop. THis is the sign he needs to believe I have spoken to you.”<br/><br/>Juan Diego ran to the top of the hill, and there, though it was winter, was a beautiful garden of Roses of Castile. Juan Diego filled his tilma with them and ran to the Church of Santiago.<br/><br/>When he arrived, he opened his tilma and poured out the fresh red blooms before the bishop. A cry came from the bishop as Juan Diego stood holding his empty tilma.<br/><br/>The bishop stared not at the roses, but at Juan Diego’s empty tilma. Imprinted there on his rough cactus fiber cloak was a painting of the Mother of God just as Juan Diego had seen her at Tepeyac.<br/><br/>A church to Our Lady of Guadalupe was built at Tepeyac, and Juan Diego made its caretaker. His tilma with its miraculous image is still there over 400 years later.<div class='entrySource'>—the story of Our Lady of Guadalupe, as told by Tomie dePaola</div>"
     },
@@ -107,7 +107,7 @@ var liturgy = [
     {
         "titleText" : "Day 13:<br/>Santa Lucia",
 
-        "imgName" : "day13.png",
+        "imgName" : "13.png",
 
         "entryText" : "St. Lucy—<br/>Plunged into night<br/>you found your name—<br/>“Light.”<br/><br/>Radiant Lady<br/>Crowned with flake<br/>So may it be with we—<br/>Illuminate!"
     },
@@ -115,7 +115,7 @@ var liturgy = [
     {
         "titleText" : "Day 14:<br/>The Raven",
 
-        "imgName" : "day14.png",
+        "imgName" : "14.png",
 
         "entryText" : "Isn't it plain the sheets of moss, except that<br/>they have no tongues, could lecture<br/>all day if they wanted about<br/><br/>spiritual patience? Isn't it clear<br/>the black oaks along the path are standing<br/>as though they were the most fragile of flowers?<br/><br/>Every morning I walk like this around<br/>the pond, thinking: if the doors of my heart<br/>ever close, I am as good as dead.<br/><br/>Every morning, so far, I'm alive. And now<br/>the crows break off from the rest of the darkness<br/>and burst up into the sky—as though<br/><br/>all night they had thought of what they would like <br/>their lives to be, and imagined<br/>their strong, thick wings.<div class='entrySource'>—“Landscape” by Mary Oliver</div>"
     },
@@ -123,7 +123,7 @@ var liturgy = [
     {
         "titleText" : "Day 15:<br/>The Goose",
 
-        "imgName" : "day15.png",
+        "imgName" : "15.png",
 
         "entryText" : "Geese appear high over us,<br/>pass, and the sky closes. Abandon,<br/>as in love or sleep, holds<br/>them to their way, clear,<br/>in the ancient faith: what we need<br/>is here. And we pray, not<br/>for new earth or heaven, but to be<br/>quiet in heart, and in eye<br/>clear. What we need is here.<div class='entrySource'>—from “The Wild Geese” by Wendell Berry</div>"
     },
@@ -131,7 +131,7 @@ var liturgy = [
     {
         "titleText" : "Day 16:<br/>The Loon",
 
-        "imgName" : "day16.png",
+        "imgName" : "16.png",
 
         "entryText" : "Loon, in winter<br/>sheds her bright plumage,<br/>becomes a grey ocean bird—<br/>flightless—drifting on the tide,<br/>feeding on whatever<br/>the tide brings to her.<div class='entrySource'>—letter from a friend</div>"
     },
@@ -139,7 +139,7 @@ var liturgy = [
     {
         "titleText" : "Day 17:<br/>The Tree",
 
-        "imgName" : "day17.png",
+        "imgName" : "17.png",
 
         "entryText" : "The wilderness and the solitary place shall be glad and the desert shall rejoice and blossom. It shall blossom abundantly and rejoice even with joy and singing.<br/><br/>Strengthen the weak hands and confirm the feeble knees, say to them that are fearful of heart—“Be strong fear not: behold your God comes!”<div class='entrySource'>—Isaiah 35:1–4</div>"
     },
@@ -147,7 +147,7 @@ var liturgy = [
     {
         "titleText" : "Day 18:<br/>The Deer",
 
-        "imgName" : "day18.png",
+        "imgName" : "18.png",
 
         "entryText" : "This time of year<br/>they slip away to the cedar yards,<br/>sheltered from blizzard<br/>and drift.<br/>If you should see one<br/>it will be between the times,<br/>not night nor yet day;<br/>between the trees she will be<br/>the angel of your childhood,<br/>your lost child,<br/>the dream you stopped<br/>dreaming,<br/>your holiness,<br/>your grace.”<div class='entrySource'>—original</div>"
     },
@@ -155,7 +155,7 @@ var liturgy = [
     {
         "titleText" : "Day 19:<br/>The Eye of God",
 
-        "imgName" : "day19.png",
+        "imgName" : "19.png",
 
         "entryText" : "The night is far spent,<br/>the day is at hand;<br/>the Holy One comes<br/>and dwells in our land.<div class='entrySource'>—interpretation of Romans 13:12</div>"
     },
@@ -163,7 +163,7 @@ var liturgy = [
     {
         "titleText" : "Day 20:<br/>The Golden Rain",
 
-        "imgName" : "day20.png",
+        "imgName" : "20.png",
 
         "entryText" : "Not a doomsday dazzle<br/>in his coming<br/>nor dark as he came;<br/>Kind, but royally reclaiming<br/>	his own;<br/>A released shower,<br/>	let flash to the shire,<br/>	not a lightning of fire<br/>hard-hurled.<div class='entrySource'>—from “The Wreck of the Deutschland” by Gerard Manley Hopkins</div>"
     },
@@ -171,7 +171,7 @@ var liturgy = [
     {
         "titleText" : "Day 21:<br/>The Fire and The Rose",
 
-        "imgName" : "day21.png",
+        "imgName" : "21.png",
 
         "entryText" : "With the drawing of this Love and the voice of this Calling<br/><br/>We shall not cease from exploration<br/>And the end of all our exploring<br/>Will be to arrive where we started<br/>And know the place for the first time.<br/>Through the unknown, unremembered gate<br/>When the last of earth left to discover<br/>Is that which was the beginning;...<br/><br/>And all shall be well and<br/>All manner of thing shall be well<br/>When the tongues of flames are in-folded<br/>Into the crowned knot of fire<br/>And the fire and the rose are one.<div class='entrySource'>—from “Little Gidding” in “The Four Quartets” by TS Eliot</div>"
     },
@@ -179,7 +179,7 @@ var liturgy = [
     {
         "titleText" : "Day 22:<br/>The Bird of Dawning",
 
-        "imgName" : "day22.png",
+        "imgName" : "22.png",
 
         "entryText" : "Some say that ever, 'gainst that season comes<br/>Wherein our Saviour's birth is celebrated,<br/>The bird of dawning singeth all night long;<br/>And then, they say, no spirit dare stir abroad,<br/>The nights are wholesome, then no planets strike,<br/>No fairy takes, nor witch hath power to charm,<br/>So hallow'd and so gracious is the time.<div class='entrySource'>—Hamlet, Act 1, Scene 1, Lines 180–187</div>"
     },
@@ -187,7 +187,7 @@ var liturgy = [
     {
         "titleText" : "Day 23:<br/>The _Animals_",
 
-        "imgName" : "day23.png",
+        "imgName" : "23.png",
 
         "entryText" : "There shall come forth a shoot from the stump of Jesse, and a branch shall grow out of its roots. Upon this one the Spirit of the Lord shall rest, the spirit of wisdom and understanding, the spirit of counsel and might, the spirit of knowledge and the fear of the Lord.<br/><br/>The wolf shall dwell with the lamb, and the leopard shall lie down with the kid, and the calf and the lion and the fatling together, and a little child shall lead them. The cow and the bear shall feed; their young shall lie down together; and the lion shall eat straw like the ox.<br/><br/>The sucking child shall play over the hole of the asp, and the weaned child shall put a hand on the adder’s den. They shall not hurt or destroy in all my holy mountain; for the earth shall be full of the knowledge of the Lord as the waters cover the sea.<div class='entrySource'>—Isaiah 11:1–2, 6–9</div>"
     },
@@ -195,7 +195,7 @@ var liturgy = [
     {
         "titleText" : "Day 24:<br/>The _Other Animals_",
 
-        "imgName" : "day24.png",
+        "imgName" : "24.png",
 
         "entryText" : "There shall come forth a shoot from the stump of Jesse, and a branch shall grow out of its roots. Upon this one the Spirit of the Lord shall rest, the spirit of wisdom and understanding, the spirit of counsel and might, the spirit of knowledge and the fear of the Lord.<br/><br/>The wolf shall dwell with the lamb, and the leopard shall lie down with the kid, and the calf and the lion and the fatling together, and a little child shall lead them. The cow and the bear shall feed; their young shall lie down together; and the lion shall eat straw like the ox.<br/><br/>The sucking child shall play over the hole of the asp, and the weaned child shall put a hand on the adder’s den. They shall not hurt or destroy in all my holy mountain; for the earth shall be full of the knowledge of the Lord as the waters cover the sea.<div class='entrySource'>—Isaiah 11:1–2, 6–9</div>"
     },
@@ -203,7 +203,7 @@ var liturgy = [
     {
         "titleText" : "Day 25:<br/>The Child",
 
-        "imgName" : "day25.png",
+        "imgName" : "25.png",
 
         "entryText" : "And the Word became flesh and dwelt among us, and we beheld his glory, the glory as of the only begotten of God, full of grace and truth.<div class='entrySource'>—John 1:14</div>"
     }
@@ -221,7 +221,8 @@ function loadLiturgy(day_num) {
 }
 
 function loadCalendar(day_num) {
-    document.getElementById('calendar-img').src = ("assets/images/" + liturgy[day_num].imgName);
+    document.getElementById('calendar-img-single').src = ("assets/images/single/" + liturgy[day_num].imgName);
+    document.getElementById('calendar-img-up-to').src = ("assets/images/up-to/" + liturgy[day_num].imgName);
 }
 
 
@@ -236,8 +237,10 @@ function dayOfAdvent() {
         day_num = today.getDate();
         console.log ("It's day " + today.getDate() + ".");
 
+    } else {
+      day_num = 0;
     }
-    day_num = 3; // TESTING OVERRIDE until all days have images and entries
+    // day_num = 3; // TESTING OVERRIDE until all days have images and entries
     return day_num;
 }
 
